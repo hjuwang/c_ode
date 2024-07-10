8fca9e203ceee789f32f7bac7d78f5af5acb073e
Bruce <hju_wang@126.com>
2024-07-10 09:52:34 +0800

    learn array and pointer

---
+++ chapter5/content/add_one.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ultral = 0, super =0;
+
+
+    while (super <5)
+    {
+        /* code */
+        //尽量不要使用
+        super++;
+        ++ultral;
+        printf("super = %d, ultral = %d\n",super,ultral);
+    }
+    
+
+    return 0;
+}
